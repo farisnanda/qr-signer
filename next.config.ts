a80961ca6fc324ceabccb9f5cda30c98e312d88f
Faris Nanda <farisnanda96@gmail.com>
2026-06-24 17:41:34 +0700

    next fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/qr-signer",
+  assetPrefix: "/qr-signer",
 };
 
 export default nextConfig;
